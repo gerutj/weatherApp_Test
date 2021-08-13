91e35c517566c676948e070e80e8c3bc6131ed55
Germán González Robles <gerutj@gmail.com>
2021-08-13 16:55:46 -0500

    Primera ver del clima con Xamarin Forms
    Version de prueba de la aplicacion del clima

---
+++ WeatherApp/WeatherApp/Converters/TextToImageConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace WeatherApp.Converters
+{
+    public class TextToImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string weather = value as string;
+            string path = weather;
+           
+           
+            Console.WriteLine("ENTRO "+(value as string));
+
+            return path;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WeatherApp/WeatherApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WeatherApp.Models;
+using Xamarin.Forms;
+
+namespace WeatherApp.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private weatherdata _data;
+
+        public weatherdata Data
+        {
+            get => _data;
+            set
+            {
+                _data = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand SearchCommand { get; set; }
+
+        public MainPageViewModel()
+        {
+            SearchCommand = new Command(async (searchTerm) =>
+            {
+                await GetData($"http://api.weatherstack.com/current?access_key=43c014d04ad81e6bd478f91ca68ba003&query={searchTerm}");
+            });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public async Task GetData(string url)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            var response =
+                await client.GetAsync(client.BaseAddress);
+            response.EnsureSuccessStatusCode();
+            var jsonResult = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<weatherdata>(jsonResult);
+
+            Data = result;
+        }
+    }
+}
